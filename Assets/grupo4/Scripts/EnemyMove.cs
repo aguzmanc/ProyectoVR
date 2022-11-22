129f1c8d490b1487e0a174156ca28d6241b8395b
ErickSebas <ume0028566@est.univalle.edu>
2022-11-22 01:31:27 -0400

    RVAvanceG4

---
+++ Assets/grupo4/Scripts/EnemyMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float runSpeed = 7f;
+    public float rotationSpeed = 250f;
+    public Animator animator;
+    [SerializeField]
+    protected Transform pos;
+    private float y;
+    private int vida = 1;
+    void Update()
+    {
+        
+
+        if(vida<=0){
+            animator.SetBool("Death", true);
+            transform.position = new Vector3(transform.position.x,0,transform.position.z);
+        }else{
+            transform.LookAt(new Vector3(pos.position.x, transform.position.y, pos.position.z));
+            y = 1;
+            transform.position = Vector3.MoveTowards(transform.position,new Vector3(pos.position.x, transform.position.y, pos.position.z), y* Time.deltaTime * runSpeed);
+            animator.SetFloat("VelY", y);
+        }
+    }
+}
