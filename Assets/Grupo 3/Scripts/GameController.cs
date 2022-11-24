8f210409dafdad2ace702b5aea58b44c574bc762
oropezalucas18 <88797809+oropezalucas18@users.noreply.github.com>
2022-11-23 21:47:39 -0400

    Mostrar puntuación en interfaz

--- Assets/Grupo 3/Scripts/GameController.cs
+++ Assets/Grupo 3/Scripts/GameController.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
     private List<GameObject> _bottles;
     private List<GameObject> _cans;
     private int _score = 0;
 
     void Start()
     {
+        scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+
         _bottles = new List<GameObject>(GameObject.FindGameObjectsWithTag("Bottle"));
         _cans = new List<GameObject>(GameObject.FindGameObjectsWithTag("Can"));
     }
@@ -20,12 +24,14 @@ public class GameController : MonoBehaviour
             {
                 _score += 10;
                 _bottles.Remove(go);
-                Debug.Log("Score " + _score);
+                Debug.Log("Score: " + _score);
+                scoreText.text = "Score: " + _score;
             }else if (_cans.Contains(go))
             {
                 _score += 20;
                 _cans.Remove(go);
                 Debug.Log("Score " + _score);
+                scoreText.text = "Score: " + _score;
             }
         }    
 
