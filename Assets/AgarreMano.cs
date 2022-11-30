f257a8f50b9af798e2e4e90734b844ac1abd2627
andyguzman7 <andyguzman117@gmail.com>
2022-11-30 09:14:14 -0400

    cambios

--- Assets/AgarreMano.cs
+++ Assets/AgarreMano.cs
@@ -8,18 +8,10 @@ public class AgarreMano : MonoBehaviour
     public GameObject child;
     public Transform parentOriginal;
     public Transform parentSustitute;
-
     bool agarreImput;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-         agarreImput = OVRInput.Get(OVRInput.Button.SecondaryHandTrigger);
-
+        agarreImput = OVRInput.Get(OVRInput.Button.SecondaryHandTrigger);
     }
 
     /// <summary>
@@ -30,22 +22,11 @@ public class AgarreMano : MonoBehaviour
     {
         if(agarreImput && other.CompareTag("Arma"))
         {
-<<<<<<< HEAD
             child.transform.SetParent(parentSustitute);
             child.transform.localPosition = new Vector3(0,0,0);
             child.transform.localRotation = Quaternion.identity;
-=======
-            child.transform.SetParent(parent);
-            other.transform.localPosition = new Vector3(0,0,0);
-            other.transform.localRotation = Quaternion.identity;
-            Debug.Log("Agarre");
->>>>>>> 3ce5a7bb0a6ce46e520cd6e771ef4e5c2b7e52d9
         }
     }
-    /// <summary>
-    /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
-    /// </summary>
-    /// <param name="other">The other Collider involved in this collision.</param>
     private void OnTriggerExit(Collider other)
     {
         if(agarreImput == false)
