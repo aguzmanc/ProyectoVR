aea62911e1590a75ce7bacb6a8ae032dcc477261
PedroConde20 <cvp2019844@est.univalle.edu>
2022-11-30 09:03:12 -0400

    Disparador

---
+++ Assets/g5/Scripts/Disparador.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparador : MonoBehaviour
+{
+    public GameObject Balas;
+    public float tiempo = 0.1f;
+    [Range(0, 20)]
+    public float rangoRotacion = 3;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(tiempo);
+
+            if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+            {
+                GameObject bullet =
+                    (GameObject)Instantiate(Balas,
+                                transform.position,
+                                transform.rotation);
+                float rotY = Random.Range(-rangoRotacion, rangoRotacion);
+                float rotX = Random.Range(-rangoRotacion, rangoRotacion);
+                bullet.transform.Rotate(rotX, rotY, 0);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
