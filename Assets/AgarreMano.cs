5efed3ee972d6ed66106221bccb4d73058fbd153
aguzmanc <spacerockganimedes@gmail.com>
2022-12-01 16:57:08 -0400

    confg

--- Assets/AgarreMano.cs
+++ Assets/AgarreMano.cs
@@ -8,20 +8,44 @@ public class AgarreMano : MonoBehaviour
     public GameObject child;
     public Transform parentOriginal;
     public Transform parentSustitute;
-    bool agarreImput;
+    
+    
+    public bool cerca;
+    public bool agarrado;
+    public bool presionado;
     void Update()
     {
-        agarreImput = OVRInput.Get(OVRInput.Button.SecondaryHandTrigger);
+        presionado = OVRInput.Get(OVRInput.Button.SecondaryHandTrigger);
 
-         if(agarreImput == false)
+        if(presionado && cerca && !agarrado)
         {
+            agarrado = true;
+            child.transform.SetParent(parentSustitute);
+            child.transform.localPosition = new Vector3(0,0,0);
+            child.transform.localRotation = Quaternion.identity;
+            Debug.Log("Agarrado");
+        }
+        
+
+        if(presionado == false && agarrado == true && cerca== true)
+        {
+            agarrado = false;
             child.transform.SetParent(parentOriginal);
             child.transform.localPosition = new Vector3(0,0,0);
             child.transform.localRotation = Quaternion.identity;
+            Debug.Log("soltado");
         }
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Arma"))
+        {
+            cerca = true;
+        }   
+        
+    }
 
-    private void OnTriggerStay(Collider other)
+    /*private void OnTriggerStay(Collider other)
     {
         if(agarreImput && other.CompareTag("Arma"))
         {
@@ -29,9 +53,13 @@ public class AgarreMano : MonoBehaviour
             child.transform.localPosition = new Vector3(0,0,0);
             child.transform.localRotation = Quaternion.identity;
         }
-    }
+    }*/
+    
     private void OnTriggerExit(Collider other)
     {
-       
+        if(other.CompareTag("Arma"))
+        {
+            cerca = false;
+        }  
     }
 }
