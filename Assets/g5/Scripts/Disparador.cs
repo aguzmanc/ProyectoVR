21a46c69471daa2d31d2dd633035e0a49f40bbdb
PedroConde20 <cvp2019844@est.univalle.edu>
2022-12-07 09:07:15 -0400

    reducir balas

--- Assets/g5/Scripts/Disparador.cs
+++ Assets/g5/Scripts/Disparador.cs
@@ -1,3 +1,4 @@
+using Oculus.Platform.Samples.VrBoardGame;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,32 @@ using UnityEngine;
 public class Disparador : MonoBehaviour
 {
     public GameObject Balas;
-    int contadorbalas;
-    
+    private int municionRifle = 30;
+
     [Range(0, 20)]
     public float rangoRotacion = 3;
     void Update()
     {
-
         if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
         {
-            GameObject bullet =
-                (GameObject)Instantiate(Balas,
-                            transform.position,
-                            transform.rotation);
-            float rotY = Random.Range(-rangoRotacion, rangoRotacion);
-            float rotX = Random.Range(-rangoRotacion, rangoRotacion);
-            bullet.transform.Rotate(rotX, rotY, 0);
-  
+            disparar();
         }
 
     }
+
+    void disparar()
+    {
+        if (municionRifle > 0)
+        {
+            GameObject bullet =
+               (GameObject)Instantiate(Balas,
+                           transform.position,
+                           transform.rotation);
+            float rotY = Random.Range(-rangoRotacion, rangoRotacion);
+            float rotX = Random.Range(-rangoRotacion, rangoRotacion);
+            bullet.transform.Rotate(rotX, rotY, 0);
+            municionRifle--;
+        }
+    }
+
 }
