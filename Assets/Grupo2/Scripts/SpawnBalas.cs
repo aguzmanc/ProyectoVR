b2e38ef83abba68b41a91c7f169bb7211823f318
AnthonyVocal <anthonyvocalhurtado@gmail.com>
2022-12-08 13:11:47 -0400

    cambios control arma

--- Assets/Grupo2/Scripts/SpawnBalas.cs
+++ Assets/Grupo2/Scripts/SpawnBalas.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OVRTouchSample;
 
 public class SpawnBalas : MonoBehaviour
 {
@@ -19,20 +20,21 @@ public class SpawnBalas : MonoBehaviour
     }
     void Update()
     {
-        /*if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch) > 0) botonDisparo = true;
-        else botonDisparo = false;*/
-        if(botonDisparo)
-            if(controladorCorrutina){
+        if ((OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch) > 0) && (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger, OVRInput.Controller.RTouch) > 0)) botonDisparo = true;
+        else botonDisparo = false;
+        if (botonDisparo)
+            if (controladorCorrutina)
+            {
                 StartCoroutine(Coroutine(segundosControlDisparo));
-            }   
+            }
     }
     IEnumerator Coroutine(int segundos)
     {
-        if(controladorDisparo && botonDisparo) Instantiate(bala, new Vector3(transform.position.x, transform.position.y,transform.position.z + 0.5f), transform.rotation);
+        if (controladorDisparo && botonDisparo) Instantiate(bala, new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.5f), transform.rotation);
         controladorCorrutina = false;
         controladorDisparo = false;
         yield return new WaitForSeconds(segundos);
         controladorDisparo = true;
         controladorCorrutina = true;
-    } 
+    }
 }
