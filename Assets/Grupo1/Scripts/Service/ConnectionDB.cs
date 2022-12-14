5a2afdc98b1820bcef0ca93e82e4deb19c632348
andyguzman7 <andyguzman117@gmail.com>
2022-12-13 22:49:57 -0400

    clases del service

---
+++ Assets/Grupo1/Scripts/Service/ConnectionDB.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Net;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace UnityPoyect
+{
+    public static class ConnectionDB
+    {
+        public static string requestUriMain = "http://192.168.33.20:5000/";
+        /*public HttpWebRequest Request { get; set; }
+        public HttpWebResponse Response { get; set; }
+        public StreamReader Reader { get; set; }
+
+        public ConnectionDB(string nameExtension)
+        {
+        
+        }*/
+
+        
+    }
+}
